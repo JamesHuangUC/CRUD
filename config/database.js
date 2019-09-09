d726e75db0db6b848653f31662f4f946f0922445
James Huang <jameshuanguc@gmail.com>
2019-09-09 16:17:17 -0700

    updated db name

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 const sequelize = require('sequelize');
 
-module.exports = new sequelize('postgres://jameshuang1996:@localhost:5432/codegig');
+module.exports = new sequelize('postgres://jameshuang1996:@localhost:5432/onlinestore');
